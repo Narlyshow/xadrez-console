735623c1647f8b41c1534a930f6c71685a9407ee
Narlson Costa <naldrums@gmail.com>
2022-08-17 01:42:26 -0300

    Melhorando impressão Tela
    Implementado método para colorir peças do tabuleiro

--- Program.cs
+++ Program.cs
@@ -9,10 +9,24 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadrez p = new PosicaoXadrez('c', 7);
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
+                tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(3, 5));
 
-            Console.WriteLine(p.toPosicao());
+                Tela.imprimirTabuleiro(tab);
+
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
             
         
         }
